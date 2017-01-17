28f8a99ae4ef31bcdeb9060e72d3d5a2bb01a968
cmu15418 <15418-staff@cs.cmu.edu>
2017-01-16 22:18:23 -0500

    Update main.cpp
    Increasing the number of runs to control variance.

--- prog1_mandelbrot_threads/main.cpp
+++ prog1_mandelbrot_threads/main.cpp
@@ -128,7 +128,7 @@ int main(int argc, char** argv) {
     //
     memset(output_serial, 0, width * height * sizeof(int));
     double minSerial = 1e30;
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 5; ++i) {
         double startTime = CycleTimer::currentSeconds();
         mandelbrotSerial(x0, y0, x1, y1, width, height, 0, height, maxIterations, output_serial);
         double endTime = CycleTimer::currentSeconds();
@@ -143,7 +143,7 @@ int main(int argc, char** argv) {
     //
     memset(output_thread, 0, width * height * sizeof(int));
     double minThread = 1e30;
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 5; ++i) {
         double startTime = CycleTimer::currentSeconds();
         mandelbrotThread(numThreads, x0, y0, x1, y1, width, height, maxIterations, output_thread);
         double endTime = CycleTimer::currentSeconds();
